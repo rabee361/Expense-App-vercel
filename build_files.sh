9cfce1c45c78da361d0d1f4b0e25c7caaae96528
rabee361 <rha60540@gmail.com>
2023-10-26 02:36:53 +0300

    changes

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+python3.x manage.py migrate 
+python3.x manage.py collectstatic
