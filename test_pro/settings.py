c28fea783bce3efa9661b718a66c373a58d5adb9
rabee361 <rha60540@gmail.com>
2023-12-05 01:27:34 +0300

    changes

--- test_pro/settings.py
+++ test_pro/settings.py
@@ -90,7 +90,11 @@ DATABASES = {
     )
 }
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
